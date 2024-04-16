7b808ec48dc4bd79b9fdc1d38b36638923ca341b
xSKUUUx <r.josoaf@gmail.com>
2024-04-16 16:13:11 +0200

    create: nav on header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,10 +1,18 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Header() {
   return (
-    <header>
-      <Link href={"/"}>Home</Link>
+    <header className="flex justify-between items-center px-5 mt-5">
+      <Link href={"/"}>Blendsk</Link>
+      <nav>
+        <Link href={"/blog"}>Blog</Link>
+        <Link href={"/award"}>Award</Link>
+      </nav>
+      <div>
+        <Link href={"/login"}>Login</Link>
+        <Link href={"/register"}>Register</Link>
+      </div>
     </header>
   );
 }
