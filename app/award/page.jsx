621ec1a47d0b695b557515da00e7deb89e79f608
xSKUUUx <r.josoaf@gmail.com>
2024-08-24 10:40:08 +0200

    making award page

--- app/award/page.jsx
+++ app/award/page.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function award() {
   return (
-    <div>award</div>
-  )
+    <div className="flex justify-center items-center w-screen h-screen">
+      <h2>Coming soon...</h2>
+    </div>
+  );
 }
