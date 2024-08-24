6761d4222966308991232d81cee2a2aae750f1e5
xSKUUUx <r.josoaf@gmail.com>
2024-08-25 01:33:11 +0200

    making signuo page

--- components/forms/SignupForm.jsx
+++ components/forms/SignupForm.jsx
@@ -1,61 +1,61 @@
-import React, { useState } from 'react'
+"use client";
+
+import React, { useState } from "react";
 
 export default function SignupForm() {
-      const [values, setValues] = useState({
-        email: "",
-        username: "",
-        email: "",
-        password: "",
-      });
+  const [values, setValues] = useState({
+    email: "",
+    username: "",
+    email: "",
+    password: "",
+  });
 
-      const handleChanges = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value });
-      };
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(values);
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
 
   return (
-    <div className="p-20">
-      <h3>Sign up to Blendsk</h3>
-      <form
-        className="flex flex-col gap-4"
-        method="POST"
-        onSubmit={handleSubmit}
-      >
-        <div className="flex">
-          <div className="flex flex-col">
-            <label htmlFor="username">Name</label>
-            <input type="text" name="name" onChange={(e) => handleChanges(e)} />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              onChange={(e) => handleChanges(e)}
-            />
-          </div>
-        </div>
-
+    <form className="flex flex-col gap-4" method="POST" onSubmit={handleSubmit}>
+      <div className="flex gap-4">
         <div className="flex flex-col">
-          <label htmlFor="password">Email</label>
-          <input type="email" name="email" onChange={(e) => handleChanges(e)} />
+          <label htmlFor="username">Name</label>
+          <input type="text" name="name" onChange={(e) => handleChanges(e)} />
         </div>
         <div className="flex flex-col">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="username">Username</label>
           <input
-            type="password"
-            name="password"
-            placeholder="6+ characters"
+            type="text"
+            name="username"
             onChange={(e) => handleChanges(e)}
           />
         </div>
+      </div>
 
-        <button type="submit">submit</button>
-      </form>
-    </div>
+      <div className="flex flex-col">
+        <label htmlFor="password">Email</label>
+        <input type="email" name="email" onChange={(e) => handleChanges(e)} />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="6+ characters"
+          onChange={(e) => handleChanges(e)}
+        />
+      </div>
+
+      <button
+        className="gap-4 p-4 rounded-full border bg-slate-950 text-slate-200 font-medium"
+        type="submit"
+      >
+        Sign up
+      </button>
+    </form>
   );
 }
