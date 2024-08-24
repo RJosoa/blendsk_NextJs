5ce23a6c1344727582e0c849906cbfc8f4714137
xSKUUUx <r.josoaf@gmail.com>
2024-08-24 10:38:17 +0200

    making Header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,17 +1,32 @@
 import Link from "next/link";
-import React from "react";
+import DarkLogo from "@/public/logo/LightmodLogo.svg";
+import Image from "next/image";
 
 export default function Header() {
+  const isLogin = false;
+  const username = "persona1";
+
   return (
-    <header className="flex justify-between items-center px-5 mt-5">
-      <Link href={"/"}>Blendsk</Link>
-      <nav>
-        <Link href={"/blog"}>Blog</Link>
+    <header className="absolute w-screen flex justify-between items-center p-5">
+      <Link href={"/"}>
+        <Image alt="DarkLogo" src={DarkLogo} width={195}/>
+      </Link>
+      <nav className="absolute left-1/2 -translate-x-1/2 flex gap-5">
+        <Link href={"/explore"}>Explore</Link>
         <Link href={"/award"}>Award</Link>
       </nav>
-      <div>
-        <Link href={"/login"}>Login</Link>
-        <Link href={"/register"}>Register</Link>
+      <div className="flex gap-5">
+        {isLogin ? (
+          <>
+            <button>Log out</button>
+            <Link href={`/${username}`}>Profile</Link>
+          </>
+        ) : (
+          <>
+            <Link href={"/login"}>Log in</Link>
+            <Link href={"/signup"}>Sign up</Link>
+          </>
+        )}
       </div>
     </header>
   );
