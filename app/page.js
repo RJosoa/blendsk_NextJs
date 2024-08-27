003326edfc7d58eef18c31bd877f1ab3d209cca3
xSKUUUx <r.josoaf@gmail.com>
2024-08-27 13:21:05 +0200

    fix vercel error: Module not found: Can't resolve '@/components/mapCard/MapCard'

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import RangeRankCarousel from "@/components/ui/carousel/RangeRankCarousel.js";
-import MapCard from "@/components/mapCard/MapCard";
+import MapCard from "@/components/ui/card/postMap/MapCard";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-screen">
-      <p className="w-full h-screen bg-red-500">HERO</p>
+      <div className="flex justify-center items-center w-full h-screen">
+        <h1>Hero</h1>
+      </div>
       <div className="flex flex-col w-full h-screen bg-cyan-500">
         <h1>Best range</h1>
         <RangeRankCarousel />
