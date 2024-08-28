0ba5b91b7e11579ec3acd0d22de6d10d8210c877
xSKUUUx <r.josoaf@gmail.com>
2024-08-28 11:06:40 +0200

    cretate authentification manager with AuthJs

--- auth.js
+++ auth.js
@@ -1,9 +1,57 @@
 import NextAuth from "next-auth";
+import bcryptjs from "bcryptjs";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import prisma from "./lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [
+    Google,
+    Credentials({
+      credentials: {
+        identifier: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        if (!credentials?.identifier || !credentials?.password) {
+          throw new Error("Missing identifier or password");
+        }
+
+        const user = await prisma.user.findFirst({
+          where: {
+            OR: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        const pepper = process.env.SECRET_PEPPER;
+        const isPasswordValid = await bcryptjs.compare(
+          credentials.password + pepper,
+          user.password
+        );
+
+        if (!isPasswordValid) {
+          throw new Error("Mot de passe incorrect");
+        }
+
+        if (!isValid) {
+          throw new Error("Invalid password");
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+      },
+    }),
+  ],
 });
